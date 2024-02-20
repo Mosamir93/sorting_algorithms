3a9d688ed3b38f04d70468bbb412e6a5a849ea65
ralzaini <rehabalzaini@gmail.com>
2024-02-20 02:37:58 -0800

    sssaw

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,18 +1,18 @@
 #include "sort.h"
 
 /**
-*swap - the positions of two elements into an array
-*@array: array
-*@a: array element
-*@b: array element
-*/
+ *swap - the positions of two elements into an array
+ *@array: array
+ *@a: array element
+ *@b: array element
+ */
 void swap(int *array, size_t a, size_t b)
 {
-    int tmp;
+	int tmp;
 
-    tmp = array[a];
-    array[a] = array[b];
-    array[b] = tmp;
+	tmp = array[a];
+	array[a] = array[b];
+	array[b] = tmp;
 }
 
 /**
@@ -25,26 +25,26 @@ void swap(int *array, size_t a, size_t b)
  */
 int hoare_partition(int *array, int first, int last, int size)
 {
-    int current = first -  1, finder = last +  1;
-    int pivot = array[last];
+	int current = first -  1, finder = last +  1;
+	int pivot = array[last];
 
-    while (1)
-    {
-        current++;
-        while (array[current] < pivot)
-        {
-            current++;
-        }
-        finder--;
-        while (array[finder] > pivot)
-        {
-            finder--;
-        }
-        if (current >= finder)
-            return (current);
-        swap(array, current, finder);
-        print_array(array, size);
-    }
+	while (1)
+	{
+		current++;
+		while (array[current] < pivot)
+		{
+			current++;
+		}
+		finder--;
+		while (array[finder] > pivot)
+		{
+			finder--;
+		}
+		if (current >= finder)
+			return (current);
+		swap(array, current, finder);
+		print_array(array, size);
+	}
 }
 
 /**
@@ -56,14 +56,14 @@ int hoare_partition(int *array, int first, int last, int size)
  */
 void quicksort(int *array, size_t first, size_t last, int size)
 {
-    size_t position =  0;
+	size_t position =  0;
 
-    if (first < last)
-    {
-        position = hoare_partition(array, first, last, size);
-        quicksort(array, first, position -  1, size);
-        quicksort(array, position, last, size);
-    }
+	if (first < last)
+	{
+		position = hoare_partition(array, first, last, size);
+		quicksort(array, first, position -  1, size);
+		quicksort(array, position, last, size);
+	}
 }
 
 /**
